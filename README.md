88aae708def6fb68819729dc8cf2f465c776a00c
Kyle Luzny <kluzny@gmail.com>
2015-08-18 13:26:48 -0500

    removed blinky cursor

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Just a small ruby script to help you learn to speed read. It takes a text file and splits it into chunks at the desired rate.
 
 ## Usage
-
 ```
-# ./sr.rb [ words per minute ] [ word group ] [ file ]
+# ruby ./sr.rb [ words per minute ] [ words at a time ] [ file ]
 
 $ ruby ./sr.rb 300 5 ./alice-in-wonderland.txt
 ```
+
+## Features
+Not a whole lot at the moment, but it works as advertised
 
 ## TODO
 Maybe I will turn this into a gem, and add features n stuff
@@ -15,7 +17,12 @@ Maybe I will turn this into a gem, and add features n stuff
 * Show the current wpm and percent complete in the top right
 * Enable Pause with space bar
 * Speed up/down with arrow keys
+* Enable quit with q, instead of ctrl + c
+* Switch from ARGV to getopt
+* Center text
+* Read from a pipe or file
+* Read from a file using a byte buffer, for exceptionally long texts
 
 ## License
 
-I pulled the book from [Project Gutenberg](https://www.gutenberg.org) and it is subject to thier license. The code is up for grabs under the GPLv3
+I pulled the book from [Project Gutenberg](https://www.gutenberg.org) and it is subject to thier license. The code is up for grabs under the [GPLv3](http://www.gnu.org/licenses/gpl.txt)
--- sr.rb
+++ sr.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
 $wpm = ARGV.shift.to_i
-$word_count = ARGV.shift.to_i
+$chunks = ARGV.shift.to_i
 $file = ARGV.shift
 
 $help = <<END
-./sr.rb [wpm] [word count] [file]
+Usage:
+  ruby ./sr.rb [ words per minute ] [ word at a time ] [ file ]
 
-ex) ./sr.rb 300 5 alice-in-wonderland.txt
+Example at a casual pace:
+  ruby ./sr.rb 300 5 ./alice-in-wonderland.txt
 END
 
 def bail(message, code = 1)
@@ -17,24 +19,39 @@ def bail(message, code = 1)
 end
 
 bail 'Please choose a wpm between 60 and 1000' unless (60..1000).include?($wpm)
-bail 'Please choose an positive integer word count' unless $word_count.is_a?(Integer) && $word_count > 0
-bail 'Please specify a wpm, word count, and file to continue' unless $wpm && $word_count && $file
+bail 'Please choose an positive integer word count' unless $chunks.is_a?(Integer) && $chunks > 0
+bail 'Please specify a wpm, word count, and file to continue' unless $wpm && $chunks && $file
 bail 'File not found', 2 unless File.exist? $file
+
+def hide_cursor
+  printf "\e[?25l"
+end
+
+def show_cursor
+  printf "\e[?25h"
+end
+
+def clear_screen
+  system 'clear'
+end
 
 words_per_second = $wpm/60.0
 seconds_per_word = 1.0/words_per_second
-timer = seconds_per_word * $word_count
+timer = seconds_per_word * $chunks
 
 words = File.read($file).split(/\s+/)
 
 begin
+  hide_cursor
   until words.empty? do
-    system 'clear'
-    puts words.slice!(0,$word_count).join(' ')
+    clear_screen
+    puts words.slice!(0,$chunks).join(' ')
     sleep timer
   end
 rescue Interrupt, StandardError => e
   puts e.message
   puts "Exiting..."
+ensure
+  show_cursor
 end
 
